70546264f33f21281c0d004bd57f2601b526244a
mikehaus <mike.haus.design@gmail.com>
2020-10-04 21:36:08 -0600

    changed main heading to fit better on mobile and started adding more functionality to nav btns

--- src/pages/home/components/home.js
+++ src/pages/home/components/home.js
@@ -1,27 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { Grid, Col } from 'rsuite';
 
 const HOME_STYLES = {
-    maincontainer: {
+    left_container: {
         left: 55,
         top: 0,
-        position: 'absolute',
+        position: 'fixed',
         height: '100vh',
-        width: '100% - 55',
+        width: '60%',
+        zIndex: -1
     },
-    headings: {
-        position: 'relative',
-        left: 10,
-        color: '#fefefe',
+    right_container: {
+        right: 0,
+        top: 0,
+        position: 'fixed',
+        height: '100vh',
+        width: '40%',
+        zIndex: -1
+    },
+    left_headings: {
+        margin: 0,
+        position: 'absolute',
+        top: '25%',
+        left: '30%',
+        transform: 'translate(-50%, -50%)',
+        color: '#b0c8d1'
+    },
+    left_top_h: {
+        color: '#de1655',
+    },
+    left_mid_h: {
+        color: '#59d0ff'
     }
 }
 class Home extends React.Component {
     render = () => {
         return(
-            <div style={HOME_STYLES.maincontainer}>
-                <div style={HOME_STYLES.headings}>
-                    <h1>Hi! I'm Mike</h1>
-                    <p>I make stuff with code.</p>
-                </div>
+            <div>
+                <Grid fluid>
+                    <Col md={12} xs={24}>
+                    <div style={HOME_STYLES.left_container}>
+                        <div style={HOME_STYLES.left_headings}>
+                            <h3>Hi!</h3>
+                            <h3 style={ HOME_STYLES.left_mid_h }>I'm Mike</h3>
+                            <p>I make stuff with code.</p>
+                        </div>
+                    </div>
+                    </Col>
+                    <Col xs={24} md={12}>
+                    <div style={HOME_STYLES.right_container}>
+
+                    </div>
+                    </Col>
+                </Grid>
             </div>
         );
     }
